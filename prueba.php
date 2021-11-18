16d5b4b24b675a5ba40b733c5a73be0b5904258c
mfcachavilano <flor.cachavilano@gmail.com>
2021-11-18 17:39:21 -0300

    se corrige funcionalidad con php

--- prueba.php
+++ prueba.php
@@ -28,3 +28,30 @@ include('conexion.php');
 
 </body>
 </html>
+
+
+<?php
+
+    include('conexion.php');
+
+    //recuperar las variables
+    $nombre=$_POST['nombre'];
+    $apellido=$_POST['apellido'];
+    $direccion_de_email=$_POST['email'];
+    $password=$_POST['pass'];
+    $repetir_password=$_POST['reppass'];
+    //hacer la sentencia de sql
+    $sql="INSERT INTO registro (nombre, apellido, direccion_de_email, password, repetir_password)
+                        values ('$nombre','$apellido','$direccion_de_email','$password','$repetir_password')";
+                                          
+    //ejecutar la sentencia de sql
+    $ejecutar=mysqli_query($conexion, $sql);
+    //verificar de la ejecucion
+    if (!$ejecutar){
+        echo utf8_decode('<script>alert("Hubo algun error en el registro, intente nuevamente")</script> ');		
+        echo "<script>location.href='registro.html'</script>";
+    }else{
+        echo utf8_decode('<script>alert("Datos cargados con éxito")</script> ');		
+        echo "<script>location.href='login.html'</script>";
+    }
+?>
